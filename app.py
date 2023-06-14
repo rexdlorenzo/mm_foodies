c0e51bedbe4e045589effd74bb26af9bae8d252b
Rex David Lorenzo <rexdavidlorenzo@gmail.com>
2023-06-14 19:29:55 +0800

    change title in main page to 'MM Foodies'

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def add_bg_from_local(image_file):
 def main():
     add_bg_from_local('background.jpg')
     # Streamlit app
-    st.title('Restaurant Recommendation Engine')
+    st.title('MM Foodies')
 
     sort_options = ['Default', 'Cuisine', 'Type of Restaurant', 'City', 'Rating']
 
